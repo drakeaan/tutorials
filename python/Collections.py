fa6cffb28496e3b9179da9321175e666eee5dd74
Dewald Slabbert <dewald.slabbert@nintex.com>
2025-04-24 12:05:01 -0700

    dict, list, tuple

--- python/Collections.py
+++ python/Collections.py
@@ -1,1 +1,24 @@
 # Dictionaries, Arrays, Lists, Tuples etc...
+
+# Tuple
+[(1,2), (3,4)]
+
+# ugh... I don't like this... it means it's basically Dictionary<object, object>()
+# so does that mean ensuring type safety?? it seems the type in python is just 'dict'.
+dictionary = {
+    "brand": "a",
+    "foo": "bar",
+    "int": 2025
+}
+
+print(len(dictionary))
+
+# similar to C#. for key, value in ...can items() be inferred? or is it required?
+for k, v in dictionary.items():
+    print(f"Key: {k} | Value: {v}")
+
+
+# type in python is just <class, 'list'>
+# ensure type safety or parsing 
+listthing = ["a", "b", "c"]
+listthing2 = ["a", 2, True]
