ec8511f0f9ba3be96c1c8f480572b34824a8b8c1
Dewald Slabbert <dewald.slabbert@nintex.com>
2025-04-24 12:36:51 -0700

    comments on objects

--- python/Collections.py
+++ python/Collections.py
@@ -4,6 +4,7 @@
 [(1,2), (3,4)]
 
 # ugh... I don't like this... it means it's basically Dictionary<object, object>()
+# e.g. <class, 'dict'>
 # so does that mean ensuring type safety?? it seems the type in python is just 'dict'.
 dictionary = {
     "brand": "a",
@@ -14,9 +15,9 @@ dictionary = {
 print(len(dictionary))
 
 # similar to C#. for key, value in ...can items() be inferred? or is it required?
+# apparently not. dictionary is the object and the enumeration is not inferred.
 for k, v in dictionary.items():
     print(f"Key: {k} | Value: {v}")
-
 
 # type in python is just <class, 'list'>
 # ensure type safety or parsing 
