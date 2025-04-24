99e9352acfb983d858f452a0992675d01e1b0a0d
Dewald Slabbert <dewald.slabbert@nintex.com>
2025-04-24 11:16:41 -0700

    some additional structs

--- azure-functions/http-trigger.py
+++ azure-functions/http-trigger.py
@@ -1,6 +1,6 @@
 
 
-
+import logging 
 import azure.functions as func
 
 app = func.FunctionApp()
@@ -12,4 +12,24 @@ app = func.FunctionApp()
 # e.g. (str, str) returnVals(str abc){}. What would that look like in python
 # also to do: get a keyboard where I and O are swopped so I can stop typing pythin
 @app.function_name(name="HttpTrigger1")
+# req --> Request object. in this case, should the Azure function HttpRequest.
 def main(req:func.HttpRequest) -> str:
+    print(req.params.get(''))
+
+    return "foo"
+
+# so same async keyword as C# and also same await keyword when calling an async method.
+# e.g. await main(blah)
+async def main(req:func.HttpRequest, ctx:func.ExecutionContex) -> func.HttpResponse:
+    logging.info('Python http trigger')
+    # can construct an object inline...
+    # TODO: determine new()
+    return func.HttpResponse( 
+            "This HTTP-triggered function executed successfully. " 
+            "Some more text.", 
+            status_code=200 
+        )
+
+# overall, the structure is similar... just syntactically slightly different (but not that much).
+# need to just figure out imports compared to usings (which objects are where..)
+# also remember which lines of code ends with : compared to C# everything ends with ;
